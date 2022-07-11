4972aec9674fef3d109e2aa5dfa15ec38d87bbfa
gregordecristoforo <gregor.decristoforo@gmail.com>
2022-07-11 16:21:07 -0400

    upsampling using cubic method

--- process_data_xarray.py
+++ process_data_xarray.py
@@ -8,7 +8,7 @@ ds = xr.load_dataset("data/short_dataset_coordinates_included.nc")
 new_x = np.linspace(ds.x[0], ds.x[-1], ds.dims["x"] * 4)
 new_y = np.linspace(ds.y[0], ds.y[-1], ds.dims["y"] * 4)
 
-ds = ds.interp(x=new_x, y=new_y)
+ds = ds.interp(x=new_x, y=new_y, method="cubic")
 del ds["x"]
 del ds["y"]
 
