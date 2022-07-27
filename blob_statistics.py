9226ac325aa62395fe6f32e442f03734760cec54
gregordecristoforo <gregor.decristoforo@gmail.com>
2022-07-27 11:02:11 -0400

    blob parameters implemented as dict

--- blob_statistics.py
+++ blob_statistics.py
@@ -3,65 +3,111 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-blob_list = pickle.load(
-    open("data/1091216028_full_data/1091216028_all_blobs.pickle", "rb")
+# blobs_quick = pickle.load(
+#     open(
+#         "data/1091216028_quick_interpolation/quick_interpolation_raft_blobs.pickle",
+#         "rb",
+#     )
+# )
+# blob_ids = [blob.blob_id for blob in blobs_quick]
+# print(f"Number of blobs: {len(blob_ids)}")
+
+high_threshold_blob_list = pickle.load(
+    open("data/1091216028_low_threshold/low_threshold_raft_blobs.pickle", "rb")
 )
-
-# blob_list = pickle.load(open("data/1091216028_full_data/1091216028_1.4_01_raft_blobs.pickle", "rb"))
-# blob_list = pickle.load(open("data/1091216028_full_data/1091216028_1.4_02_raft_blobs.pickle", "rb"))
-# blob_list = pickle.load(open("data/1091216028_1.45_raft_blobs.pickle", "rb"))
-print(blob_list)
-
-blob_ids = [blob.blob_id for blob in blob_list]
+blob_ids = [blob.blob_id for blob in high_threshold_blob_list]
 print(f"Number of blobs: {len(blob_ids)}")
 
-lifetimes = [blob.life_time for blob in blob_list]
+low_threshold_blob_list = pickle.load(
+    open("data/1091216028_1.45_raft_blobs.pickle", "rb")
+)
 
-for blob in blob_list:
-    blob.smoothen_all_parameters()
-    blob.remove_blobs_outside_of_SOL()
+blob_ids = [blob.blob_id for blob in low_threshold_blob_list]
+print(f"Number of blobs: {len(blob_ids)}")
 
-lifetimes = [blob.life_time for blob in blob_list]
-amplitudes = [blob.amplitudes for blob in blob_list]
-velocities_x = [blob.velocities_x for blob in blob_list]
-velocities_R = [blob.velocities_R for blob in blob_list]
-velocities_y = [blob.velocities_y for blob in blob_list]
-velocities_Z = [blob.velocities_Z for blob in blob_list]
-sizes = [blob.sizes for blob in blob_list]
-widths_x = [blob.width_x for blob in blob_list]
-widths_y = [blob.width_y for blob in blob_list]
-widths_R = [blob.width_R for blob in blob_list]
-widths_Z = [blob.width_Z for blob in blob_list]
-centers_of_mass_x = [blob._centers_of_mass_x[0] for blob in blob_list]
+blob_dict_high = {}
+blob_dict_low = {}
 
-vx_max = [np.max(v) for v in velocities_x if len(v) > 0]
-vx_mean = [np.mean(v) for v in velocities_x if len(v) > 0]
+for (dictionary, blob_list) in zip(
+    [blob_dict_low, blob_dict_high], [high_threshold_blob_list, low_threshold_blob_list]
+):
+    dictionary["lifetimes"] = [blob.life_time for blob in blob_list]
 
-vR_max = [np.max(v) for v in velocities_R if len(v) > 0]
-vR_mean = [np.mean(v) for v in velocities_R if len(v) > 0]
+    dictionary["lifetimes"] = [blob.life_time for blob in blob_list]
+    dictionary["amplitudes"] = [blob.amplitudes for blob in blob_list]
+    dictionary["velocities_x"] = [blob.velocities_x for blob in blob_list]
+    dictionary["velocities_R"] = [blob.velocities_R for blob in blob_list]
+    dictionary["velocities_y"] = [blob.velocities_y for blob in blob_list]
+    dictionary["velocities_Z"] = [blob.velocities_Z for blob in blob_list]
+    dictionary["sizes"] = [blob.sizes for blob in blob_list]
+    dictionary["widths_x"] = [blob.width_x for blob in blob_list]
+    dictionary["widths_y"] = [blob.width_y for blob in blob_list]
+    dictionary["widths_R"] = [blob.width_R for blob in blob_list]
+    dictionary["widths_Z"] = [blob.width_Z for blob in blob_list]
+    dictionary["centers_of_mass_x"] = [blob._centers_of_mass_x[0] for blob in blob_list]
 
-vy_max = [np.max(v) for v in velocities_y if len(v) > 0]
-vy_mean = [np.mean(v) for v in velocities_y if len(v) > 0]
+    dictionary["vx_max"] = [np.max(v) for v in dictionary["velocities_x"] if len(v) > 0]
+    dictionary["vx_mean"] = [
+        np.mean(v) for v in dictionary["velocities_x"] if len(v) > 0
+    ]
 
-amp_max = [np.max(amp) for amp in amplitudes if len(amp) > 1]
-amp_mean = [np.mean(amp) for amp in amplitudes if len(amp) > 1]
+    dictionary["vR_max"] = [np.max(v) for v in dictionary["velocities_R"] if len(v) > 0]
+    dictionary["vR_mean"] = [
+        np.mean(v) for v in dictionary["velocities_R"] if len(v) > 0
+    ]
 
-size_max = [np.max(size) for size in sizes if len(size) > 1]
-size_mean = [np.mean(size) for size in sizes if len(size) > 1]
+    dictionary["vy_max"] = [np.max(v) for v in dictionary["velocities_y"] if len(v) > 0]
+    dictionary["vy_mean"] = [
+        np.mean(v) for v in dictionary["velocities_y"] if len(v) > 0
+    ]
 
-width_x_max = [np.max(width_R) for width_R in widths_x if len(width_R) > 1]
-width_x_mean = [np.mean(width_R) for width_R in widths_x if len(width_R) > 1]
+    dictionary["vZ_max"] = [np.max(v) for v in dictionary["velocities_Z"] if len(v) > 0]
+    dictionary["vZ_mean"] = [
+        np.mean(v) for v in dictionary["velocities_Z"] if len(v) > 0
+    ]
 
-width_R_max = [np.max(width_R) for width_R in widths_R if len(width_R) > 1]
-width_R_mean = [np.mean(width_R) for width_R in widths_R if len(width_R) > 1]
+    dictionary["amp_max"] = [
+        np.max(amp) for amp in dictionary["amplitudes"] if len(amp) > 1
+    ]
+    dictionary["amp_mean"] = [
+        np.mean(amp) for amp in dictionary["amplitudes"] if len(amp) > 1
+    ]
 
-width_y_max = [np.max(width_Z) for width_Z in widths_y if len(width_Z) > 1]
-width_y_mean = [np.mean(width_Z) for width_Z in widths_y if len(width_Z) > 1]
+    dictionary["size_max"] = [
+        np.max(size) for size in dictionary["sizes"] if len(size) > 1
+    ]
+    dictionary["size_mean"] = [
+        np.mean(size) for size in dictionary["sizes"] if len(size) > 1
+    ]
 
-width_Z_max = [np.max(width_Z) for width_Z in widths_Z if len(width_Z) > 1]
-width_Z_mean = [np.mean(width_Z) for width_Z in widths_Z if len(width_Z) > 1]
+    dictionary["width_x_max"] = [
+        np.max(width_R) for width_R in dictionary["widths_x"] if len(width_R) > 1
+    ]
+    dictionary["width_x_mean"] = [
+        np.mean(width_R) for width_R in dictionary["widths_x"] if len(width_R) > 1
+    ]
 
+    dictionary["width_R_max"] = [
+        np.max(width_R) for width_R in dictionary["widths_R"] if len(width_R) > 1
+    ]
+    dictionary["width_R_mean"] = [
+        np.mean(width_R) for width_R in dictionary["widths_R"] if len(width_R) > 1
+    ]
 
-plt.hist(width_R_mean, bins=64)
-plt.hist(width_x_mean, bins=64)
+    dictionary["width_y_max"] = [
+        np.max(width_Z) for width_Z in dictionary["widths_y"] if len(width_Z) > 1
+    ]
+    dictionary["width_y_mean"] = [
+        np.mean(width_Z) for width_Z in dictionary["widths_y"] if len(width_Z) > 1
+    ]
+
+    dictionary["width_Z_max"] = [
+        np.max(width_Z) for width_Z in dictionary["widths_Z"] if len(width_Z) > 1
+    ]
+    dictionary["width_Z_mean"] = [
+        np.mean(width_Z) for width_Z in dictionary["widths_Z"] if len(width_Z) > 1
+    ]
+
+plt.hist(blob_dict_low["lifetimes"], bins=64)
+plt.hist(blob_dict_high["lifetimes"], bins=64)
 plt.show()
