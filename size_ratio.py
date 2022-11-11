35c51b06b1a3ae2d05d0c1b71f942f82e8fc797e
gregordecristoforo <gregor.decristoforo@gmail.com>
2022-11-11 15:23:55 +0100

    size ratio estimates corrested by superposition term

--- size_ratio.py
+++ size_ratio.py
@@ -30,18 +30,29 @@ for (dictionary, blob_list) in zip(dict_list, blob_lists):
     dictionary["com_y"] = [blob._centers_of_mass_y for blob in blob_list]
     dictionary["width_x"] = [blob.width_x for blob in blob_list]
     dictionary["width_y"] = [blob.width_y for blob in blob_list]
-    dictionary['size_ratio'] = [np.array(blob.width_Z)/np.array(blob.width_R) for blob in blob_list]
+    dictionary["sizes"] = [blob.sizes for blob in blob_list]
+    dictionary['size_ratio'] = [np.maximum(np.array(blob.width_Z),np.array(blob.width_R))/np.minimum(np.array(blob.width_Z),np.array(blob.width_R)) for blob in blob_list]
+    dictionary['superposition_ratio'] =  [np.array(blob.width_Z)*np.array(blob.width_R)/np.array(blob.sizes) for blob in blob_list]
+    dictionary['elongation'] = np.array(dictionary["size_ratio"]) * np.array(dictionary['superposition_ratio'])
 
+mean_elongation_rf_on = [np.mean(blob) for blob in rf_on["elongation"]]
+mean_elongation_rf_off = [np.mean(blob) for blob in rf_off["elongation"]]
+number_blobs_rf_on = np.arange(len(mean_elongation_rf_on))
+number_blobs_rf_off = np.arange(len(mean_elongation_rf_off))
 
-mean_ratios_rf_on = [np.mean(blob) for blob in rf_on["size_ratio"]]
-mean_ratios_rf_off = [np.mean(blob) for blob in rf_off["size_ratio"]]
-number_blobs_rf_on = np.arange(len(mean_ratios_rf_on))
-number_blobs_rf_off = np.arange(len(mean_ratios_rf_off))
-
-plt.hist(mean_ratios_rf_off, label='RF off',alpha=0.5,density=True, range=(0,5),bins=32)
-plt.hist(mean_ratios_rf_on, label='RF on', alpha=0.5,density=True,range=(0,5),bins=32)
-plt.ylabel(r'$P(\overline{\delta_Z}/\overline{\delta_R})$')
-plt.xlabel(r'$\overline{\delta_Z}/\overline{\delta_R}$')
+plt.hist(mean_elongation_rf_off, label='RF off',alpha=0.5,density=True, range=(0,5),bins=32)
+plt.hist(mean_elongation_rf_on, label='RF on', alpha=0.5,density=True,range=(0,5),bins=32)
+plt.ylabel('P(elongation)')
+plt.xlabel('elongation')
 plt.legend()
 plt.savefig("blob_elongation_hist.pdf",bbox_inches="tight")
+plt.show()
+
+plt.figure()
+plt.plot(mean_elongation_rf_off,'o', label='RF off', alpha=0.5)
+plt.plot(mean_elongation_rf_on, 'o',label='RF on',alpha = 0.5)
+plt.legend()
+plt.xlabel('Blob ID')
+plt.ylabel('elongation')
+plt.savefig("blob_elongation_scatter.pdf",bbox_inches="tight")
 plt.show()
